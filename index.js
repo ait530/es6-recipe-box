3641e5f6a5f00a3cca588d4dd679219f916370ff
Aaron I Trierweiler <ait530@users.noreply.github.com>
2018-05-30 17:48:52 -0500

    pushing entries to an array

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const userInput = () => {
-  let userIngredientInput = document.getElementById('recipe-form').value;
-  document.getElementById('addedIngredients').innerHTML = "The user input is: " + userIngredientInput;
+const userIngredientInput = () => {
+
+  let arrayOfIngredientsData = [];
+  
+  let userTextInput = document.getElementById('recipe-form').value;
+  
+  let addedIngredient =document.getElementById('addedIngredients');
+
+  addedIngredient.innerHTML = "The user input is: " + userTextInput;
+
+  arrayOfIngredientsData.push(userTextInput);
+  
+  console.log(arrayOfIngredientsData);
 };
   
-  
+
+
