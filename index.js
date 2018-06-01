d6a5ac03198f8bce6e4058aa6ecf697653a67dbe
Aaron I Trierweiler <ait530@users.noreply.github.com>
2018-06-01 13:07:46 -0500

    instructions function added as well as callback fucntion

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
+let arrayOfIngredientsData = ['Chicken', 'Noodles', 'Broccoli'];
+
 const userIngredientInput = () => {
-  let arrayOfIngredientsData = [];
   let userTextInput = document.getElementById('recipe-form').value;
   let recipeAddButton = document.getElementById(
     'recipe-add-button');
   let addedIngredient = document.getElementById('addedIngredients');
 
-  if (userTextInput !== "") {
+  if (userTextInput !== '') {
     let entry = document.createElement('li');
     entry.appendChild(document.createTextNode(userTextInput));
     addedIngredient.appendChild(entry);
   
     arrayOfIngredientsData.push(userTextInput);
     console.log(arrayOfIngredientsData);
+
+    afterInitialInput();
   }
-  else if (userTextInput === "" && arrayOfIngredientsData.length === 0) {
-    alert('Please enter a recipe item');
+  else if (userTextInput === '' && arrayOfIngredientsData.length > 3) {
+    alert('Please input a recipe item');
   }
+  else  {
+    instructions();
+  }
+};
+
+const instructions = () => {
+  alert('Welcome to the Recipe Box app! Please enter a recipe item to get started.');
+};
+
+const afterInitialInput = (userTextInput) => {
+    console.log(arrayOfIngredientsData.length);
 };
 
 window.onload = userIngredientInput;
