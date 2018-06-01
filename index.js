ced926eee46d7dab5bdb460034d2532c601e6261
Aaron I Trierweiler <ait530@users.noreply.github.com>
2018-06-01 02:29:01 -0500

    text entries append to added ingredient section

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const userIngredientInput = () => {
-  let arrayOfIngredientsData = []
+  let arrayOfIngredientsData = [];
   let userTextInput = document.getElementById('recipe-form').value;
-  let addedIngredient =document.getElementById('addedIngredients');
+  let recipeAddButton = document.getElementById(
+    'recipe-add-button');
+  let addedIngredient = document.getElementById('addedIngredients');
 
   if (userTextInput !== "") {
-    addedIngredient.innerHTML = "The user input is: " + userTextInput;
+    let entry = document.createElement('li');
+    entry.appendChild(document.createTextNode(userTextInput));
+    addedIngredient.appendChild(entry);
+  
     arrayOfIngredientsData.push(userTextInput);
-
     console.log(arrayOfIngredientsData);
-  };
+  }
+  else if (userTextInput === "" && arrayOfIngredientsData.length === 0) {
+    alert('Please enter a recipe item');
+  }
 };
 
 window.onload = userIngredientInput;
