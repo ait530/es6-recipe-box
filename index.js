e8a455771ac7833a19cc20c88402490cce9edd2f
Aaron I Trierweiler <ait530@users.noreply.github.com>
2018-06-01 01:24:45 -0500

    window.onload added

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const userIngredientInput = () => {
-
   let arrayOfIngredientsData = []
   let userTextInput = document.getElementById('recipe-form').value;
   let addedIngredient =document.getElementById('addedIngredients');
 
-  addedIngredient.innerHTML = "The user input is: " + userTextInput;
-  arrayOfIngredientsData.push(userTextInput);
+  if (userTextInput !== "") {
+    addedIngredient.innerHTML = "The user input is: " + userTextInput;
+    arrayOfIngredientsData.push(userTextInput);
 
-  for (let i = 0; i<10; i++) {
-    console.log(arrayOfIngredientsData[i]);
-  }
-  console.log(arrayOfIngredientsData);
+    console.log(arrayOfIngredientsData);
+  };
 };
-  
 
-
+window.onload = userIngredientInput;
