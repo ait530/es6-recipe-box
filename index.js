60a94d85e4ea4ab526ee0615763a2a25b4e40b38
Aaron I Trierweiler <ait530@users.noreply.github.com>
2018-05-31 23:46:36 -0500

    code cleanup

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const userIngredientInput = () => {
 
-  let arrayOfIngredientsData = [];
-  
+  let arrayOfIngredientsData = []
   let userTextInput = document.getElementById('recipe-form').value;
-  
   let addedIngredient =document.getElementById('addedIngredients');
 
   addedIngredient.innerHTML = "The user input is: " + userTextInput;
-
   arrayOfIngredientsData.push(userTextInput);
-  
+
+  for (let i = 0; i<10; i++) {
+    console.log(arrayOfIngredientsData[i]);
+  }
   console.log(arrayOfIngredientsData);
 };
   
