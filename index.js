746f255d092438c9e9209297586897ed2662b653
Aaron I Trierweiler <ait530@users.noreply.github.com>
2018-06-13 00:27:08 -0500

    code cleanup

--- index.js
+++ index.js
@@ -1,52 +1,46 @@
-const arrayOfIngredientsData = [];
+const arrayOfIngredientsData = []
 
 const userIngredientInput = () => {
-  let userTextInput = document.getElementById('recipe-form').value;
+  let userTextInput = document.getElementById('recipe-form').value
   let recipeAddButton = document.getElementById(
-    'recipe-add-button'); 
+    'recipe-add-button')
 
   if (userTextInput !== '') {
-    arrayOfIngredientsData.push(userTextInput);
-    console.log(arrayOfIngredientsData);
+    arrayOfIngredientsData.push(userTextInput)
+    console.log(arrayOfIngredientsData)
 
-    afterInput(userTextInput);
+    afterInput(userTextInput)
+  } else if (userTextInput === '' && arrayOfIngredientsData.length > 3) {
+    alert('Please input a recipe item')
+  } else {
+    startInstructions()
   }
-  else if (userTextInput === '' && arrayOfIngredientsData.length > 3) {
-    alert('Please input a recipe item');
-
-  }
-  else  {
-    startInstructions();
-  }
-};
+}
 
 const startInstructions = () => {
-  alert('Welcome to the Recipe Box app! Please enter a recipe item to get started.');
-};
+  alert('Welcome to the Recipe Box app! Please enter a recipe item to get started.')
+}
 
 const afterInput = (userTextInput) => {
-    const filteredIngredientsDataArray = [];
-    let addedIngredient = document.getElementById('addedIngredients');
-    const ingredientsData = arrayOfIngredientsData.filter(function(item, index) {
-      
-      if (arrayOfIngredientsData.indexOf(item) == index){
-        filteredIngredientsDataArray.push(item); 
-      }
-      else if (arrayOfIngredientsData.indexOf(item) !== index) {
-        return;
-        alert('Please enter a different ingredient');
-      }
-    });
-    
-    console.log(userTextInput);
-    console.log(filteredIngredientsDataArray.slice(-1)[0]);
+  const filteredIngredientsDataArray = []
+  let addedIngredient = document.getElementById('addedIngredients')
+  const ingredientsData = arrayOfIngredientsData.filter(function (item, index) {
+    if (arrayOfIngredientsData.indexOf(item) === index) {
+      filteredIngredientsDataArray.push(item)
+    } else if (arrayOfIngredientsData.indexOf(item) !== index) {
+      alert('Please enter a different ingredient')
+    }
+  })
 
-    let entry = document.createElement('li');
-    if (userTextInput === filteredIngredientsDataArray.slice(-1)[0]){
-      entry.appendChild(document.createTextNode(filteredIngredientsDataArray.slice(-1)[0])); 
-      addedIngredient.appendChild(entry);
-      console.log(filteredIngredientsDataArray);  
-    } 
-};
+  console.log(userTextInput)
+  console.log(filteredIngredientsDataArray.slice(-1)[0])
 
-window.onload = userIngredientInput;
+  let entry = document.createElement('li')
+  if (userTextInput === filteredIngredientsDataArray.slice(-1)[0]) {
+    entry.appendChild(document.createTextNode(filteredIngredientsDataArray.slice(-1)[0]))
+    addedIngredient.appendChild(entry)
+    console.log(filteredIngredientsDataArray)
+  }
+}
+
+window.onload = userIngredientInput
